332e046f1dcde1629e4aec192893be982d22feab
sandraMA87 <sandra.moas@hotmail.com>
2023-04-01 15:13:08 +0000

    contador

--- src/js/component/SecondCounter.jsx
+++ src/js/component/SecondCounter.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
 function SecondCounter({ seconds }) {
-  return (
-    <div className="contador">
+  
+  const horas = Math.floor(seconds / 3600).toString().padStart(2, "0");
+  const minutos = Math.floor((seconds % 3600) / 60).toString().padStart(2, "0");
+  const segundos = (seconds % 60).toString().padStart(2, "0");
 
-      <p>{0}:</p>
-      <p>{0}:</p>
-      <p>{0}:</p>
-      <p>{0}:</p>
-      <p>{minutes}:</p>
-      <p>{seconds}</p>
+  return (
+    <div className="contador">&#x23F3;
+      <div className="">{horas[0]}</div>
+      <div className="">{horas[1]}:</div>
+      <div className="">{minutos[0]}</div>
+      <div className="">{minutos[1]}:</div>
+      <div className="">{segundos[0]}</div>
+      <div className="">{segundos[1]}</div>
     </div>
   );
-}
+};
 
 export default SecondCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -6,32 +6,14 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
 //import your own components
-import Home from "./component/home.jsx";
 import SecondCounter from "./component/SecondCounter.jsx";
-import MinutesCounter from "./component/MinutesCounter.jsx";
-import HoursCounter from "./component/HoursCounter.jsx";
 
 let seconds = 0;
-let minutes = 0;
-let hours = 0;
-//render your react application
+
 setInterval(()=> {
-
-    seconds++
-
-    if (seconds === 60) {
-        seconds = 0;
-        minutes++;
-    }
-
-    if (minutes === 60) {
-        minutes = 0;
-        hours++;
-    }
-ReactDOM.render( <div>
-    <SecondCounter seconds={seconds} />
-    <MinutesCounter minutes={minutes} />
-    <HoursCounter hours={hours} />
-</div>, document.querySelector("#app"));
+   seconds++
+ReactDOM.render( 
+    <SecondCounter seconds={seconds} />, document.querySelector("#app"));
 }, 1000);
